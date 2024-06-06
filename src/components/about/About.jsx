dcc87a64760e2f0f7fda2c196ed815d510a1c607
Abduboriy <asqarovabduboriy8@gmail.com>
2024-06-06 13:18:11 +0500

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,18 +1,38 @@
 import React from "react";
-import './About.css'
+import "./About.css";
 
 const About = () => {
+  let skils = ['html',"css",'sass','js','react.js','git','bootstrap','tailwind','typescript','netx.js']
+  let skills = skils.map((skill,i) => <div className="skill_div" key={i}>{skill}</div>)
   return (
     <>
       <section id="about">
         <div className="container">
-            <div className="text_about">
-                <h1>About Me</h1>
-                <div className="underline"></div>
-                <p>
-                Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology
-                </p>
+          <div className="text_about">
+            <h1>About Me</h1>
+            <div className="underline"></div>
+            <p>
+              Here you will find more information about me, what I do, and my
+              current skills mostly in terms of programming and technology
+            </p>
+          </div>
+
+          <div className="flex_about">
+            <div className="about_me">
+              <h2>Get to know me!</h2>
+              <p>
+                I am a frontend developer. I deal with the front end of websites
+                and web applications. Check out this portfolio to learn more
+                about me!
+              </p>
             </div>
+            <div className="skills">
+              <h2>My Skills</h2>
+               <div className="skkils_container">
+                 {skills}
+               </div>
+            </div>
+          </div>
         </div>
       </section>
     </>
